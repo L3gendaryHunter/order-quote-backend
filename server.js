acf62a24f72c330fa3a67b59b532bd43e0eca619
L3gendaryHunter <117578076+L3gendaryHunter@users.noreply.github.com>
2025-05-18 22:51:28 +0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+app.get('/api/fetchProduct', async (req, res) => {
+  const url = req.query.url;
+
+  // Example using Rainforest API (replace with your own key and service)
+  const response = await axios.get('https://api.rainforestapi.com/request', {
+    params: {
+      api_key: 'your_rainforest_api_key',
+      type: 'product',
+      url: url
+    }
+  });
+
+  const product = response.data.product;
+  res.json({
+    name: product.title,
+    price_usd: product.buybox_winner?.price?.value || product.price.value
+  });
+});
+
+app.listen(3000, () => console.log('Server running')); */
